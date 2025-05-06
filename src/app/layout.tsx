8fb2313d81f02a2a40d4e6a8b7c959f70b59aa67
micheliuuu <micheliu@stanford.edu>
2025-05-06 10:47:24 -0700

    slight nav bar restructuing to fix TRPCReach and Clerk authentication conflict

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,6 @@ export default function RootLayout({
       <html lang="en" className={`${geist.variable}`}>
         <body>
           <TRPCReactProvider>
-            <Navbar />
             {children}
           </TRPCReactProvider>
           <Toaster />
