7cf5c1234c0a446e6b388d2d05f1b70ec318ef5e
micheliuuu <micheliu@stanford.edu>
2025-05-19 11:16:27 -0700

    Added Dashboard title to the dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -53,11 +53,15 @@ export default async function DashboardPage() {
   return (
     <>
       <Navbar />
-      <Dashboard
-        initialCurrentUser={dbUser}
-        initialCommunities={communitiesForDashboard}
-        initialMyRequests={myRequests}
-      />
+      <main className="container mx-auto px-4 py-8" >
+      <h1 className="text-3xl font-bold mb-6 ml-44">Dashboard</h1>
+        <Dashboard
+        
+          initialCurrentUser={dbUser}
+          initialCommunities={communitiesForDashboard}
+          initialMyRequests={myRequests}
+        />
+      </main>
     </>
   );
 }
